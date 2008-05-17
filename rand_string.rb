cb0c526ef35b40db50571c9d8a87e29aa534bde5
Caius Durling <caius@julius.local>
2008-05-17 23:22:02 +0100

    Adding new script

---
+++ rand_string.rb
@@ -0,0 +1,21 @@
+# Generates a random string of length x
+# using a-z A-Z 0-9
+
+# length of the random string
+LENGTH = 35
+
+chars = ( (0..9).to_a | ('a'..'z').to_a | ('A'..'Z').to_a ).map {|x| x.to_s }
+
+class Array
+  def random_element
+    self.sort_by { Kernel.rand }.first
+  end
+end
+
+a = []
+
+until a.length == LENGTH
+  a << chars.random_element
+end
+
+puts a.join # => "TbN7u5GlZpKDBMxKGOLMhD27VwMOU3RcpEM"
